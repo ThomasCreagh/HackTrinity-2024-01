2c18d3f2eda687d8bb49c719b76073d546d5777d
ThomasCreagh <github@thomascreagh.mailer.me>
2024-10-12 13:18:30 +0100

    getting response

--- frontend/main.js
+++ frontend/main.js
@@ -5,7 +5,6 @@
   console.log(JSON.stringify({ websiteUrl: tab.url }));
   console.log(typeof (tab.url));
 
-
   fetch("http://localhost:8000/api/website-assessment/", {
     // mode: "no-cors",
     method: "POST",
@@ -16,7 +15,11 @@
       "Content-type": "application/json; charset=UTF-8"
     }
   })
-    .then(response => {
-      console.log(response.json())
+    .then(response => response.json())
+    .then(data => {
+      paragraph.appendChild(document.createTextNode(JSON.stringify(data)));
     })
+    .catch(error => {
+      console.log("ERROR:\n", error);
+    });
 })();
