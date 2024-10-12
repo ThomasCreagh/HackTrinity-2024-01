663e3b1aeb333e9cd32769ed0b794d3b032e744a
Michal <118948097+bronickm@users.noreply.github.com>
2024-10-12 16:31:50 +0100

    integrated scraping with the API

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import AnyUrl, BaseModel
 
 from assessment import assess_policy
+from scrape import get_tos
 
 
 class WebsiteAssessmentRequest(BaseModel):
@@ -22,8 +23,7 @@ app.add_middleware(
 
 @app.post("/api/website-assessment/")
 async def assess_website(request: WebsiteAssessmentRequest):
-    with open('sample_policy.txt', 'r', encoding="utf-8") as file:
-        text = file.read()
+    text = get_tos(str(request.websiteUrl))
     
     results = assess_policy(text)
     filtered_results = [item for item in results if item["result"] == True]
